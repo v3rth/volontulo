cfcae66cd32f4c93f5b66bc356932dfefee3319d
v3rth <free4q@gmail.com>
2018-08-09 12:31:37 +0200

    980 Add tests for offer-header component

--- frontend/src/app/offers/offer-header/offer-header.component.spec.ts
+++ frontend/src/app/offers/offer-header/offer-header.component.spec.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs/Observable';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OfferHeaderComponent } from './offer-header.component';
@@ -5,12 +6,27 @@ import {IconLabelComponent} from "../../icon-label/icon-label.component";
 import {RouterTestingModule} from "@angular/router/testing";
 import {IconComponent} from "../../icon/icon.component";
 import {UserService} from "../../user.service";
+import {Offer} from "../../homepage-offer/offers.model";
 
 describe('OfferHeaderComponent', () => {
-  let component: OfferHeaderComponent;
-  let fixture: ComponentFixture<OfferHeaderComponent>;
+  const editButtonCaption = 'Edytuj ofertę';
 
-  beforeEach(async(() => {
+  let component: OfferHeaderComponent;
+  let editButton: Element|null;
+  let fixture: ComponentFixture<OfferHeaderComponent>;
+  let userService: jasmine.SpyObj<UserService>;
+
+  function buildComponent() {
+    fixture = TestBed.createComponent(OfferHeaderComponent);
+    component = fixture.componentInstance;
+    component.offer = new Offer();
+    fixture.detectChanges();
+    editButton = fixture.nativeElement.querySelector('a.btn');
+  }
+
+  beforeEach(async (() => {
+    const userServiceMock = jasmine.createSpyObj('UserService', ['canUserEditOffer']);
+
     TestBed.configureTestingModule({
       imports: [ RouterTestingModule ],
       declarations: [
@@ -19,19 +35,33 @@ describe('OfferHeaderComponent', () => {
         IconComponent,
       ],
       providers: [
-        { provide: UserService, useValue: {} }
+        { provide: UserService, useValue: userServiceMock }
       ]
-    })
-    .compileComponents();
+    });
+
+    userService = TestBed.get(UserService);
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(OfferHeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => buildComponent());
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy(); //TODO
-  // });
+  it('should hide edit button by default', () => {
+    expect(editButton).toBeNull();
+  });
+
+  it('should hide edit button for non authorized user', () => {
+    expect(editButton).toBeNull();
+  });
+
+  it('should show edit button for authorized user', () => {
+    userService.canUserEditOffer.and.returnValue(Observable.of(true));
+
+    buildComponent();
+
+    expect(editButton).not.toBeNull();
+    expect(editButton.innerHTML.toString()).toContain(editButtonCaption);
+  });
 });
